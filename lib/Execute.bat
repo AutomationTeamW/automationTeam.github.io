3bd25b0295267989093dd62becc487e8b0c8138c
VSlavova <ivanka.slavova@wacom.com>
2019-08-14 13:09:06 +0300

    pom.xml

---
+++ lib/Execute.bat
@@ -0,0 +1,5 @@
+set projectPath=C:\Users\SlavovaIvanka\IdeaProjects\testwacomv
+cd C:\Users\SlavovaIvanka\IdeaProjects\testwacomv
+set classpath=C:\Users\SlavovaIvanka\IdeaProjects\testwacomv\bin;C:\Users\SlavovaIvanka\IdeaProjects\testwacomv\lib\*
+java org.testng.TestNGC:\Users\SlavovaIvanka\IdeaProjects\testwacomv\testng.xml
+pause
---
+++ lib/executev2.bat
@@ -0,0 +1,3 @@
+cd C:\Users\SlavovaIvanka\IdeaProjects\testwacomv
+java -cp C:\Users\SlavovaIvanka\IdeaProjects\testwacomv\lib\*;C:\Users\SlavovaIvanka\IdeaProjects\testwacomv\bin org.testng.TestNGtestng.xml
+pause
